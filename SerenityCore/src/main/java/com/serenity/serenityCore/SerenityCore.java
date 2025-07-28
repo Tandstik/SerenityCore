550e01097c9738c437e772a12c5ca3e08cf54e54
Tandstik <115925462+Tandstik@users.noreply.github.com>
2025-07-28 19:02:47 +0300

    Update SerenityCore.java

--- SerenityCore/src/main/java/com/serenity/serenityCore/SerenityCore.java
+++ SerenityCore/src/main/java/com/serenity/serenityCore/SerenityCore.java
@@ -1,21 +1,19 @@
 package com.serenity.serenityCore;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.event.Listener;
 
 public final class SerenityCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        System.out.println(ChatColor.GREEN + "SerenityCore has been enabled!");
+        getLogger().info("SerenityCore has been enabled!");
         saveDefaultConfig();
         Bukkit.getPluginManager().registerEvents(new PlayerListener(this), this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("SerenityCore has been disabled!");
     }
 }
